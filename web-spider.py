4bcfdd0658500faaa34c6ed7ae354baf10ff5f7b
Sabrina-Jiang <sabrina_jiang@outlook.com>
2016-11-21 21:41:52 +0800

    visualiztion

--- web-spider.py
+++ web-spider.py
@@ -1,45 +1,12 @@
-# from pyquery import PyQuery as pq
-
-
-# link_tree = []
-# org_tree = []
-
-# # d = pq(url=_domain)
-# # d('a').each(lambda i, item: link_tree.append({"url": pq(item).attr('href'), "title": d('title').text(), "next": []}) )
-
-# # link_tree = list(filter(lambda node: (node['url'] != None and (_domain in node['url'] or node['url'][0] == "/")), link_tree))
-
-# # _domain in pq(item) or pq(item)[0] == "/"
-# # pq(item) != None
-
-# def dive(target_node, target_url):
-#     global target_node
-#     if not target_url in link_tree:
-#         link_tree.append(target_url)
-#     tree_node = []
-    
-#     if not _domain in target_url:
-#         target_url = _domain + target_url
-
-#     d = pq(url=target_url)
-#     d('a').each(lambda i, item: tree_node.append({"url": pq(item).attr('href'), "title": d('title').text(), "next": []}))
-#     tree_node = list(filter(lambda node: (node['url'] != None) and (_domain in node['url'] or node['url'][0] == "/"), tree_node))
-    
-#     for i in tree_node:
-#         dive(i['next'], i['url'])
-#         print(tree_node)
-    
-
-# dive(org_tree, _domain)
-
-
-
-from pyquery import PyQuery as pq 
+from pyquery import PyQuery as pq
+from graphviz import Digraph
+ 
 import hashlib
 import queue
 
+dot = Digraph(comment='Sitemap')
+
 _domain = "http://www.umass.edu"
-q = queue.Queue()
 
 url_pool = []
 graph = {}
@@ -62,16 +29,19 @@ def dive(target_url):
         
 
         for i in result:
-            print('Adding Url Releation between ' + target_url + " and " + i['url'])
+            if not _domain in i['url']:
+                item_url = _domain + i['url'] 
+            
+            print('Adding Url Releation between ' + target_url + " and " + item_url)
             graph[url_hash].append(i)
-            q.put(i['url'])
+            dot.node(hashlib.md5(item_url.encode('utf-8')).hexdigest(), item_url)
+            dot.edge(hashlib.md5(target_url.encode('utf-8')).hexdigest(), hashlib.md5(item_url.encode('utf-8')).hexdigest())
         
         url_pool.append(url_hash)
 
     except Exception as e:
         print(e)
 
-q.put(_domain)
+dive(_domain)
 
-while q.qsize() > 0:
-    dive(q.get())
+dot.view()
